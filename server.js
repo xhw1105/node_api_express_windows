83051cbf205e2dd6d1584f22f3d1004088707fb9
shshchch88 <shshchch88@hotmail.com>
2020-09-11 11:09:44 +1200

    added routes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
