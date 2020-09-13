13b25f8459617c491810e40237938810eca61ccb
shshchch88 <shshchch88@hotmail.com>
2020-09-13 21:25:38 +1200

    added error handler for CastError.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,26 +1,46 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Bootcamp = require('../models/Bootcamp');
 
 // @desc        Get all bootcamps
 // @route       GET /api/v1/bootcamps
 // @access      Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, message: bootcamps });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // @desc        Get a specified bootcamp
 // @route       GET /api/v1/bootcamps/:id
 // @access      Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, message: 'Show a bootcamp' });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    if (!bootcamp) {
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      );
+    }
+    res.status(200).json({ success: true, message: bootcamp });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // @desc        Create a bootcamp
 // @route       GET /api/v1/bootcamps
 // @access      Private
 exports.createBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.create(req.body);
-  res.status(201).json({
-    success: true,
-    data: bootcamp,
-  });
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
